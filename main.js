b0fd51e2a3e46b2b545ef57d48db05e5973884d0
Ilia Selieznov <ziziron21@gmail.com>
2019-11-01 02:29:21 +0200

    commit new update links

---
+++ main.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    document.execCommand("copy");
+}
+function myFunction2() {
+    var copyNumber = document.getElementById("myInputNumber");
+    copyNumber.select();
+    document.execCommand("copy");
+}
+function myFunctionSkype() {
+    var copySkype = document.getElementById("myInputSkype");
+    copySkype.select();
+    document.execCommand("copy");
+}
